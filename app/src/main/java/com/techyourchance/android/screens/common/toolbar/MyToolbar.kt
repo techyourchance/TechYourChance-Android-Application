b56c9563accbecfc2dc7fd33edf7d2d254dd1084
Haris Dautovic <haris.dautovic.dev@gmail.com>
2023-12-28 15:43:00 +0100

    Remove unused import

--- app/src/main/java/com/techyourchance/android/screens/common/toolbar/MyToolbar.kt
+++ app/src/main/java/com/techyourchance/android/screens/common/toolbar/MyToolbar.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.TextView
-import androidx.appcompat.widget.SwitchCompat
 import androidx.core.view.isVisible
 import com.techyourchance.android.R
 
